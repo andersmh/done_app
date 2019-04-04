7f06296c6750d0d8a861d66639a7c4e5cb9f122e
571559 <31239471+andersmh@users.noreply.github.com>
2019-04-05 00:25:44 +0200

    changed date

--- lib/util/date_formatter.dart
+++ lib/util/date_formatter.dart
@@ -2,6 +2,6 @@ import 'package:intl/intl.dart';
 
 String dateFormatted() {
   var now = DateTime.now();
-  var formatter = new DateFormat("EEEE H:m d/M/y");
+  var formatter = new DateFormat("EEEE HH:mm d/M/y");
   return formatter.format(now);
 }
